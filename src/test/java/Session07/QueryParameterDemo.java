3ed1156433264f1bde92b08ccb0eb8f19a318e7f
prachicodestudio <prachicodechannel@gmail.com>
2023-02-16 20:03:06 +0530

    session 13

--- src/test/java/Session07/QueryParameterDemo.java
+++ src/test/java/Session07/QueryParameterDemo.java
@@ -4,9 +4,13 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.specification.QueryableRequestSpecification;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.SpecificationQuerier;
 
 
 public class QueryParameterDemo {
@@ -16,30 +20,56 @@ public class QueryParameterDemo {
 	{
 		//Get Request Specification for the given request
 		RequestSpecification requestSpec = RestAssured.given();
-		
+
 		//specify url
 		requestSpec.baseUri("https://reqres.in");
 		requestSpec.basePath("/api/users");
 		requestSpec.queryParam("page", 2).queryParam("id", 10);
-		
+
+
+		//query details from request specification
+
+		QueryableRequestSpecification queryRequest = SpecificationQuerier.query(requestSpec);
+
+		//get base URI
+		String retrieveBaseURI = queryRequest.getBaseUri();
+		System.out.println("Base URI:" + retrieveBaseURI);
+
+		//get base PATH
+		String retrieveBasePath = queryRequest.getBasePath();
+		System.out.println("Base Path:" + retrieveBasePath);
+
+		//get request Headers
+
+		Headers allHeaders = queryRequest.getHeaders();
+		System.out.println("\n----------------REQUEST HEADER---------------------\n");
+		for(Header h:allHeaders)
+		{
+			System.out.println("Header name:" + h.getName() + "\tHeader value:" + h.getValue());
+		}
+
+
+
+
+
 		//perform get request
 		Response response =requestSpec.get();
-		
+
 		//read response body
-		 String responseBodyString = response.getBody().asString();
-		 
-		 //print response body
-		 System.out.println("Response body:" + responseBodyString );
-	
-		 //get json path view of response body
+		String responseBodyString = response.getBody().asString();
+
+		//print response body
+		System.out.println("Response body:" + responseBodyString );
+
+		//get json path view of response body
 		JsonPath jsonPathView = response.jsonPath();
-		
+
 		//get first_name
 		//x.data.first_name
 		String firstname = jsonPathView.get("data.first_name");
-		
+
 		Assert.assertEquals(firstname,"prachi","Verify firstname.");
-		
-		
+
+
 	}
 }
---
+++ src/test/java/session13/QueryRequestSpecification.java
@@ -0,0 +1,68 @@
+package session13;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
+import io.restassured.specification.QueryableRequestSpecification;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.SpecificationQuerier;
+
+public class QueryRequestSpecification {
+
+	//https://reqres.in/api/users
+
+	@Test
+	public void createUser()
+	{
+		//create json data
+		JSONObject jsonData = new JSONObject();
+		jsonData.put("name", "Rekha");
+		jsonData.put("job", "QA");
+
+
+		//Create request specification
+		RequestSpecification reqSpec = RestAssured.given();
+
+		//specify url
+		reqSpec.baseUri("https://reqres.in").
+		basePath("/api/users").
+		contentType(ContentType.JSON).
+		body(jsonData.toJSONString()).header("header1","header1value");
+
+
+		//query details from request specification
+
+		QueryableRequestSpecification queryRequest = SpecificationQuerier.query(reqSpec);
+
+		//get base URI
+		String retrieveBaseURI = queryRequest.getBaseUri();
+		System.out.println("Base URI:" + retrieveBaseURI);
+
+		//get base PATH
+		String retrieveBasePath = queryRequest.getBasePath();
+		System.out.println("Base Path:" + retrieveBasePath);
+
+
+		//get request body
+		String retrieveRequestBody = queryRequest.getBody();
+		System.out.println("Body :" + retrieveRequestBody);
+		
+		//get request Headers
+		
+		Headers allHeaders = queryRequest.getHeaders();
+		System.out.println("\n----------------REQUEST HEADER---------------------\n");
+		for(Header h:allHeaders)
+		{
+			System.out.println("Header name:" + h.getName() + "\tHeader value:" + h.getValue());
+		}
+		
+		
+		
+		
+
+	}
+}
